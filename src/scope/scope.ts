8c208717ef1baa690f4fd53ef5f346a17eb8c7a8
Mike Lysikov <d8corp@mail.ru>
2021-06-22 11:07:21 +0300

    added TODO

--- src/scope/scope.ts
+++ src/scope/scope.ts
@@ -2,7 +2,7 @@ import {TPluginsObject} from '..'
 
 const scope = {
   currentPlugins: {} as TPluginsObject,
-  references: {} as Record<symbol, Node>
+  references: {} as Record<symbol, Node> // TODO: change to WeakMap
 }
 
 export default scope
