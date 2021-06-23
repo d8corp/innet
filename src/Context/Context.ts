c47d4cb6fa6de7b90b9189ba4aabcf0990e65c8b
Mike Lysikov <d8corp@mail.ru>
2021-06-23 16:32:23 +0300

    improved Context types

--- src/Context/Context.ts
+++ src/Context/Context.ts
@@ -2,19 +2,19 @@ import {Content, NodeContext} from '..'
 import scope from '../scope'
 
 /** You can pass a value from a parent element through any children to the place you need. */
-class Context <T = any> {
+class Context <T = any, V = T extends undefined ? any : T> {
   key = Symbol('context')
 
-  constructor (public defaultValue: T) {}
+  constructor (public defaultValue?: T) {}
 
-  provide (value: T, children: Content): NodeContext {
+  provide (value: V, children: Content): NodeContext {
     return {
       children,
       context: {__proto__: scope.currentPlugins, [this.key]: value},
     }
   }
 
-  get value () {
+  get value (): V {
     const {currentPlugins} = scope
     const {key} = this
     return key in currentPlugins ? currentPlugins[key] : this.defaultValue
